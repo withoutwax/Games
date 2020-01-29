4b9ea0c9cf32b3e50994f1fbec8675a9e7da393a
Will <rlagmlckd@gmail.com>
2020-01-29 01:34:48 -0600

    .

---
+++ build/precache-manifest.3020fe9fe6524dd6f79e8db0683f7d04.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a71bbf813dae2bdf6d51fcaea71fd26",
+    "url": "/Games/index.html"
+  },
+  {
+    "revision": "ce04cb617e310364c1f3",
+    "url": "/Games/static/css/main.490c4314.chunk.css"
+  },
+  {
+    "revision": "eb402540b14f82b5d42d",
+    "url": "/Games/static/js/2.abed1086.chunk.js"
+  },
+  {
+    "revision": "5586a95853ee43deda1fb4da37dc3fc8",
+    "url": "/Games/static/js/2.abed1086.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce04cb617e310364c1f3",
+    "url": "/Games/static/js/main.0c08167a.chunk.js"
+  },
+  {
+    "revision": "100f015e3fe515c4ad0b",
+    "url": "/Games/static/js/runtime-main.8497562b.js"
+  },
+  {
+    "revision": "072bab2f2fa9e69d9a89492fc73d07ba",
+    "url": "/Games/static/media/04b03.072bab2f.woff2"
+  },
+  {
+    "revision": "0ddae1c3bca61b76636f59f49a64e5c4",
+    "url": "/Games/static/media/04b03.0ddae1c3.svg"
+  },
+  {
+    "revision": "3ecc686a74db32ecd9babd5a8d01c11e",
+    "url": "/Games/static/media/04b03.3ecc686a.woff"
+  },
+  {
+    "revision": "60af8f8582580174a78cb2dba0785f46",
+    "url": "/Games/static/media/04b03.60af8f85.eot"
+  },
+  {
+    "revision": "85c85652c98a2da2c24006562398ce1c",
+    "url": "/Games/static/media/04b03_.85c85652.ttf"
+  },
+  {
+    "revision": "3a97e5392f22a71f7a48cbfc3604af90",
+    "url": "/Games/static/media/click.3a97e539.png"
+  },
+  {
+    "revision": "4993166b5fb8edce3e169aae3138328c",
+    "url": "/Games/static/media/joystick.4993166b.png"
+  },
+  {
+    "revision": "3a97e5392f22a71f7a48cbfc3604af90",
+    "url": "/Games/static/media/snake.3a97e539.png"
+  }
+]);
